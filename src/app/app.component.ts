b27bd37cacd7d1f4944c4b474ed6d7fa11745987
gvieiraschwade <guilherme.vieiraschwade@amadeus.com>
2019-05-15 09:36:43 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kata-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'kata-simon-says-rxjs';
+}
